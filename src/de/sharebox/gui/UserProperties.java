b05b7e4e925a18829bd92f2e75f2a7dd3eb59b0b
mawil <wilbersmartin@googlemail.com>
2011-06-17 14:01:58 +0200

    viewAktualisiert

---
+++ src/de/sharebox/gui/UserProperties.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package de.sharebox.gui;
+
+import de.sharebox.controller.Controller;
+
+/**
+ * @author MW
+ * 
+ */
+// class in which the user can change the properties
+public class UserProperties extends ChangeablePanel {
+
+
+	
+	
+	
+	// every GUI class has to have a UID to verify
+	private static final long serialVersionUID = 2342342341L;
+
+	
+	public UserProperties(Controller c){
+		
+		super(c);
+	}
+	
+	
+}
